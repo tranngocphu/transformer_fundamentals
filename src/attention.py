19c61d22eefe45bd4fbc1a20a8c36c68e8819e68
Phu Tran <tr.ngocphu@gmail.com>
2025-03-27 13:54:32 -0400

    import inputs

--- src/attention.py
+++ src/attention.py
@@ -93,11 +93,8 @@ class MultiheadAttention(nn.Module):
     
 
 if __name__ == "__main__":
-    batch_size = 4
-    seq_len = 64
-    d_model = 512
-    num_heads = 8
-    mh_attn = MultiheadAttention(num_heads, d_model)    
+    from inputs import batch_size, num_heads, seq_len, d_model
+    mh_attn = MultiheadAttention(num_heads, d_model)
     x = torch.randn((batch_size, seq_len, d_model))    
     y = mh_attn(x, x, x)  
     assert list(y.shape) == [batch_size, seq_len, d_model], \
