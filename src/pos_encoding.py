15fbc42e06a8a01b114d37dcbc3b6abb9f03da1d
Phu Tran <tr.ngocphu@gmail.com>
2025-03-27 12:42:19 -0400

    positional encoding

---
+++ src/pos_encoding.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+import torch
+import math
+
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.max_len = max_len
+        
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1) # shape [max_len, 1]
+        div_term = torch.exp( torch.arange(0, d_model, 2) * - (math.log(1000.0) / d_model) )  # shape [d_model//2]
+        pe[:, 0::2] = torch.sin(position * div_term) # assign sin() to even positions
+        pe[:, 1::2] = torch.cos(position * div_term) # assign cos() to odd positions
+        pe = pe.unsqueeze(0)  # shape [1, max_len, d_model]
+        self.register_buffer("pe", pe)
+        
+    def forward(self, x):
+        x = x + self.pe[:, :x.size(1)].requires_grad_(False)
+        return x
+    
+    
+    
+if __name__ == "__main__":
+    from inputs import d_model, x, batch_size, seq_len
+    pe_encoder = PositionalEncoding(d_model)
+    x_p = pe_encoder(x)
+    assert list(x_p.shape) == [batch_size, seq_len, d_model], \
+        f"Shape {x_p.shape} mismatched! Expecting {[batch_size, seq_len, d_model]}."
+    
+        
+        
+        
