7d58c32c2feca06969a03e2902dcba372be2de50
Phu Tran <tr.ngocphu@gmail.com>
2025-03-27 00:02:29 -0400

    add multihead attention

--- src/attention.py
+++ src/attention.py
@@ -28,11 +28,64 @@ def attention(query, key, value, mask=None, dropout=None):
     if dropout is not None:
         p_attn = dropout(p_attn)
 
-    # get the result        
+    # get the result, shape will be [batch_size, seq_len, d_k]
     result = torch.matmul(p_attn, value)
         
     return result, p_attn
-    
+
+
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiheadAttention, self).__init__()
+        # d_model must be divisible by h
+        assert d_model % h == 0
+        self.d_k = d_model // h
+        self.h = h
+        self.dropout = dropout
+        self.linear_q = nn.Linear(d_model, d_model)
+        self.linear_k = nn.Linear(d_model, d_model)
+        self.linear_v = nn.Linear(d_model, d_model)
+        self.linear_out = nn.Linear(d_model, d_model)
+        self.attn = None
+
+    def forward(self, query, key, value, mask=None):
+        """_summary_
+
+        Args:
+            query (_type_): shape of [batch_size, d_model]
+            key (_type_): shape of [batch_size, d_model]
+            value (_type_): shape of [batch_size, d_model]
+            mask (_type_, optional): _description_. Defaults to None.
+        """
+        if mask is not None:
+            # expand one more dimention to account for multiple heads
+            # as the same mask will apply to all heads
+            mask = mask.unsqueeze(0)
         
-    
-    
+        batch_size = query.size(0)     
+        
+        # Linear projection of Q, K, V, creating h heads afterwards
+        # note that d_k * self.h = d_model
+        # and we are adding a dimension to account for h heads
+        # new shape: [batch_size, h, seq_len, d_k]
+        query = self.linear_q(query).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+        key   = self.linear_k(key).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+        value = self.linear_v(value).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+        
+        # Apply attention
+        # x is final self-attention of shape [batch_size, h,  seq_len, d_k], 
+        # self.attn is scaled attention map of shape [batch_size, h,  seq_len, seq_len]
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+        
+        # Concat all heads and apply linear projection
+        # new shape: [batch_size, seq_len, d_model]
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_k)
+        
+        # clean up
+        del query
+        del key
+        del value
+        
+        return self.linear_out(x)
+      
