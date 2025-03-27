e633eea65c0ca61ac0636588ceeae156f90fe697
Phu Tran <tr.ngocphu@gmail.com>
2025-03-27 13:07:01 -0400

    fix comments

--- src/pos_encoding.py
+++ src/pos_encoding.py
@@ -13,8 +13,8 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1) # shape [max_len, 1]
         div_term = torch.exp( torch.arange(0, d_model, 2) * - (math.log(1000.0) / d_model) )  # shape [d_model//2]
-        pe[:, 0::2] = torch.sin(position * div_term) # assign sin() to even positions
-        pe[:, 1::2] = torch.cos(position * div_term) # assign cos() to odd positions
+        pe[:, 0::2] = torch.sin(position * div_term) # assign sin() to even embedding indices
+        pe[:, 1::2] = torch.cos(position * div_term) # assign cos() to odd embedding incides
         pe = pe.unsqueeze(0)  # shape [1, max_len, d_model]
         self.register_buffer("pe", pe)
         
