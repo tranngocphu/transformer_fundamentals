aef67b2f5d7049246b4e02c1f3eb48cfe756597d
Phu Tran <tr.ngocphu@gmail.com>
2025-03-28 00:22:29 -0400

    test successful

--- src/test.py
+++ src/test.py
@@ -9,16 +9,14 @@ def inference_test():
     src_mask = torch.ones(1, 1, 10)
 
     memory = test_model.encode(src, src_mask)      
-    ys = torch.zeros(1, 1).type_as(src.data)
+    ys = torch.zeros(1, 1).type_as(src.data)  # start token
 
     for i in range(9):
         out = test_model.decode( memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src) )
         prob = test_model.generator(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.data[0]
-        ys = torch.cat(
-            [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
-        )
+        _, next_word = torch.max(prob, dim=1)  # get index of max prob
+        next_word = next_word.data[0]        
+        ys = torch.cat( [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1 )
 
     print("Example Untrained Model Prediction:", ys)
 
@@ -30,3 +28,16 @@ def run_tests():
         
 if __name__ == "__main__":
     run_tests()
+    
+    # Expected output:
+    #    
+    # Example Untrained Model Prediction: tensor([[0, 4, 4, 4, 2, 4, 4, 4, 4, 4]])
+    # Example Untrained Model Prediction: tensor([[ 0, 10,  1,  0,  0, 10,  1,  0, 10,  1]])
+    # Example Untrained Model Prediction: tensor([[ 0,  6,  8,  7,  6,  2,  4,  2,  4, 10]])
+    # Example Untrained Model Prediction: tensor([[0, 4, 2, 2, 9, 7, 9, 6, 3, 7]])
+    # Example Untrained Model Prediction: tensor([[0, 7, 7, 7, 7, 7, 7, 7, 7, 7]])
+    # Example Untrained Model Prediction: tensor([[ 0, 10,  4,  0, 10,  4,  0, 10,  4,  0]])
+    # Example Untrained Model Prediction: tensor([[ 0,  6, 10,  6, 10,  5, 10,  6, 10,  6]])
+    # Example Untrained Model Prediction: tensor([[0, 3, 0, 3, 7, 0, 3, 3, 3, 0]])
+    # Example Untrained Model Prediction: tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    # Example Untrained Model Prediction: tensor([[0, 2, 6, 3, 3, 3, 3, 3, 3, 3]])
